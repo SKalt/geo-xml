d753805f98e8d09e5a7b0522eb5e06818f64b2d9
Steven Kalt <steven@kalt.cloud>
2024-11-02 17:16:48 -0400

    fix(scripts): insure relative imports in docstrings use package names

--- scripts/include.js
+++ scripts/include.js
@@ -1,8 +1,39 @@
 // @ts-check
 
 /** include snapshot tests in tsdoc comments */
-import { readFile } from 'node:fs/promises';
+import { readFile, stat } from 'node:fs/promises';
 import { resolve, parse, join } from 'node:path';
+async function exists(file) {
+  try {
+    const f = await stat(file);
+    return f.isFile();
+  } catch (err) {
+    return false;
+  }
+}
+/**
+ * @param {string} file
+ */
+async function getPackageName(file) {
+  // walk up the directory tree until we find a package.json
+  const dir = parse(file).dir;
+  if (dir === '/') throw new Error('Cannot find package.json');
+
+  const pkg = resolve(join(dir, 'package.json'));
+  if (!(await exists(pkg))) {
+    return await getPackageName(dir);
+  }
+  /** @type string */
+  const text = await readFile(pkg, 'utf8');
+  if (!text) throw new Error(`${pkg} is empty`);
+
+  try {
+    return JSON.parse(text)['name'];
+  } catch (err) {
+    console.error('Error parsing package.json', { text });
+    throw err;
+  }
+}
 
 const markerPattern = /^[/][*]!! use-example file:[/][/]([^ *]+)\s*[*][/]/;
 const startExample = /^```ts/;
@@ -12,13 +43,14 @@ const endExample = /^```/;
  * @param {string} file
  */
 async function getMarkers(file) {
+  const pkgName = await getPackageName(file);
   const dir = parse(file).dir;
   const text = await readFile(file, 'utf8');
   /** @type { "seek-marker" | "seek-example" | "replace" } */
   let mode = 'seek-marker';
   let output = '';
   let currentExample = '';
-  const lines = text.split('\n');
+  const lines = text.trimEnd().split('\n');
   for (let i = 0; i < lines.length; i++) {
     let line = lines[i] + '\n';
     if (mode === 'seek-marker') {
@@ -35,21 +67,23 @@ async function getMarkers(file) {
       if (match) mode = 'replace';
     } else if (mode === 'replace') {
       if (endExample.test(line)) {
-        output += await transformExample(currentExample); // TODO: transform
+        output += await transformExample(currentExample, pkgName); // TODO: transform
         output += line;
         mode = 'seek-marker';
       }
     }
   }
 
-  console.log(output);
+  return output;
 }
 
 /**
  *
  * @param {string} file an absolute path to a file
+ * @param {string} pkgName
  */
-const transformExample = async (file) => {
+const transformExample = async (file, pkgName) => {
+  // assume the directory structure is src/*.ts, tests/*.ts, tests/snapshots/*
   const text = await readFile(file, 'utf8');
   const expected = [...text.matchAll(/toMatchFileSnapshot/g)];
   const snap = /[.]toMatchFileSnapshot[(][^'"]*['"]([^'"]+)['"][^)]*[)]/gm;
@@ -72,7 +106,7 @@ const transformExample = async (file) => {
       const xml = files[snapshotPath];
       return '.toBe(' + prettyString(xml, '  ', 0, 2) + '\n  )';
     })
-    .replaceAll('../src', 'geojson-to-gml-3/src');
+    .replaceAll('../src', pkgName + '/src');
   return result;
 };
 
@@ -119,7 +153,8 @@ async function main(file) {
     process.exit(1);
   }
   // TODO: search, sort src/*.ts files
-  await getMarkers(file);
+  const output = await getMarkers(file);
+  console.log(output.trimEnd()); // console.log re-adds the trailing newline
 }
 
 main(process.argv[2]);
